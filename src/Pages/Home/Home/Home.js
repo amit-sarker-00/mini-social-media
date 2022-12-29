7bc88d3a0279763f6e78d2153fb03537e7e002fc
Amit Sarker <amitsarker255@gmail.com>
2022-12-29 19:15:20 +0600

    home page post added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HomePage from "../HomePage/HomePage";
-import NewPost from "../NewPost/NewPost";
+import ViralPost from "../ViralPost/ViralPost";
 
 const Home = () => {
   return (
     <div>
       <HomePage></HomePage>
+      <ViralPost></ViralPost>
     </div>
   );
 };
